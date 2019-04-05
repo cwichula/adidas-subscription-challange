d6def5268fc1d845bce5ecb272fdf0a0bc61b9ce
Tadeusz Cwichula <cwichula@gmail.com>
2019-04-05 18:54:52 +0200

    installation bat

--- reinstall_mail.bat
+++ reinstall_mail.bat
@@ -2,8 +2,11 @@
 
 call mvnw clean package dockerfile:build -DskipTests
 
-kubectl delete -f kubernetes\mail.yml
 kubectl delete -f kubernetes\mail-service.yml
-kubectl create -f kubernetes\mail.yml
-kubectl create -f kubernetes\mail-service.yml
+timeout 5 > NUL
+kubectl delete -f kubernetes\mail.yml
+timeout 5 > NUL
+kubectl apply -f kubernetes\mail.yml
+timeout 5 > NUL
+kubectl apply -f kubernetes\mail-service.yml
 
